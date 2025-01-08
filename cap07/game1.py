947a1ad754d6891485b82b1ea84ff39cf7b76bbd
isaque524 <isaquesilvadamasceno@gmail.com>
2025-01-08 10:30:44 -0300

    first commit

---
+++ cap07/game1.py
@@ -0,0 +1,58 @@
+import random
+from os import system, name
+
+def limpa_tela():
+
+    if name == 'nt':
+        _= system('cls')
+
+    else:
+        _= system('clear')
+
+def game():
+
+    limpa_tela()
+
+    print("\nBem-vindo(a) ao jogo da forca!")
+    print("Adicinhe a palavra abaixo:\n")
+
+    palavras = ['banana','abacate','uva','morango','laranja']
+
+    palavra = random.choice(palavras)
+
+    letras_descorbertas = ['_' for letra in palavra]
+
+    chances = 6
+
+    letras_erradas = []
+
+    while chances >0:
+
+        print(" ".join(letras_descorbertas))
+        print("\nChances restantes:", chances)
+        print("Letras erradas:"," ".join(letras_erradas))
+
+        tentativa = input("\nDigite uma letra: ").lower()
+
+        if tentativa in palavra:
+            index = 0
+
+            for letra in palavra:
+                if tentativa == letra:
+                    letras_descorbertas[index] = letra
+                index +=1
+        else:
+            chances-= 1
+            letras_erradas.append(tentativa)
+
+        if "_" not in letras_descorbertas:
+            print("\nVocê venceu, a palavra era:", palavra)
+            break
+
+        if "_" in letras_descorbertas:
+            print("\nVocê perdeu, a palavra era:", palavra)
+
+
+if __name__ == "__main__":
+    game()
+    print("\nParabens. e bla bla bla bla. :)\n")
